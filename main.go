8382044810e8e8020d77e4709d9400f06e6a05f8
taiyou <hiromacha1116@icloud.com>
2023-12-22 16:37:18 +0900

    corsライブラリのインストール

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	e := gin.Default()
+	e.Use(cors.Default())
+
 	e.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"status": "success",
+			"status":  "success",
+			"message": "Hello World",
 		})
 	})
 
