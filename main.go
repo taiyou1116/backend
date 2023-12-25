ca7d2d70cc01cc624c6a9429b7cde806aa2f2608
taiyou <hiromacha1116@icloud.com>
2023-12-25 21:44:27 +0900

    usersテーブルの取得

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	// DB情報
+	const (
+		host     = "db"
+		port     = 5432
+		user     = "user"
+		password = "password"
+		dbname   = "mydatabase"
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
 	e := gin.Default()
 	e.Use(cors.Default())
 
@@ -14,6 +43,29 @@ func main() {
 			"status":  "success",
 			"message": "Hello World",
 		})
+	})
+
+	// ここでusersテーブルからデータを取得するルートを追加
+	e.GET("/users", func(c *gin.Context) {
+
+		rows, err := db.Query("SELECT id, username, email FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []gin.H
+		for rows.Next() {
+			var id int
+			var username, email string
+			err = rows.Scan(&id, &username, &email)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, gin.H{"id": id, "username": username, "email": email})
+		}
+
+		c.JSON(200, gin.H{"users": users})
 	})
 
 	e.Run(":8000")
